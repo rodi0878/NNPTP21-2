5c954c3f3e5d6949b609bf20fa1128d517be0730
Karel Andres <kajusuandr97@gmail.com>
2021-11-06 11:15:45 +0100

    Rename parameters at PasswordDatabase class, make file and password final. Implements methods load and save at PasswordDatabase class.

--- src/main/java/cz/upce/fei/nnptp/zz/entity/PasswordDatabase.java
+++ src/main/java/cz/upce/fei/nnptp/zz/entity/PasswordDatabase.java
@@ -6,6 +6,7 @@
 package cz.upce.fei.nnptp.zz.entity;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.List;
 
 /**
@@ -13,23 +14,27 @@ import java.util.List;
  * @author Roman
  */
 public class PasswordDatabase {
-    private File file;
-    private String passwd;
+    private final File file;
+    private final String password;
     
     private List<Password> passwords;
 
-    public PasswordDatabase(File file, String passwd) {
+    public PasswordDatabase(File file, String password) {
         this.file = file;
-        this.passwd = passwd;
+        this.password = password;
     }
     
-    public void load() {
-        // TODO: use JSON and CryptoFile to load
-        // TODO: throw exceptions when error
+    public void load() throws FileNotFoundException {
+        String passwordsInString = CryptoFile.readFile(file, password);
+        if(passwordsInString == null){
+            throw new FileNotFoundException("Something at reading file was wrong, look at log to more informations.");
+        }
+        passwords = new JSON().fromJson(passwordsInString);
     }
     
     public void save() {
-        // TODO: use JSON and CryptoFile t save
+        String passwordsContext = new JSON().toJson(passwords);
+        CryptoFile.writeFile(file, password, passwordsContext);
     }
     
     public void add(Password password) {
