32eaf8048892f0651f9267ed11373120a88dd4db
lev-michael <michael.lev@hrsd.cz>
2021-11-08 14:10:16 +0100

    Add support for all parameters in toJson() method

--- src/main/java/cz/upce/fei/nnptp/zz/entity/JSON.java
+++ src/main/java/cz/upce/fei/nnptp/zz/entity/JSON.java
@@ -6,31 +6,42 @@
 package cz.upce.fei.nnptp.zz.entity;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  *
  * @author Roman
  */
 public class JSON {
-    
-    
-    public String toJson(List<Password> passwords)  {
-        // TODO: support all parameters!!!
-        String output = "[";
+
+    public String toJson(List<Password> passwords) {
+        StringBuilder output = new StringBuilder("[");
         for (Password password : passwords) {
-            if (!output.isEmpty() && !output.equals("["))
-                output += ",";
-            output += "{";
-            output += "id:" + password.getId() + ",";
-            output += "password:\"" + password.getPassword()+"\"";
-            
-            output += "}";
+            if (!output.toString().equals("[")) {
+                output.append(",");
+            }
+            output.append("{");
+            output.append("id:");
+            output.append(password.getId());
+            output.append(",");
+            output.append("password:\"");
+            output.append(password.getPassword());
+            output.append("\"");
+            if (password.getParameters() != null) {
+                for (Map.Entry<String, Parameter> parameter : password.getParameters().entrySet()) {
+                    output.append(",");
+                    output.append(parameter.getKey());
+                    output.append(":\"");
+                    output.append(parameter.getValue());
+                    output.append("\"");
+                }
+            }
+            output.append("}");
         }
-        output += "]";
-        
-        return output;
+        output.append("]");
+        return output.toString();
     }
-    
+
     public List<Password> fromJson(String json) {
         throw new RuntimeException("NYI");
     }
